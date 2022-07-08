8fc163da6e873c7d6ab050edc95ba535f877def0
partha7978 <muduliparthasarathi@gmail.com>
2022-07-08 19:57:49 +0530

    adding copy to clipboard function when clicking on copy icon

--- script.js
+++ script.js
@@ -2,9 +2,19 @@ var color1 = document.querySelector(".color1");
 var color2 =  document.querySelector(".color2");
 var css = document.querySelector("h3");
 var body = document.getElementById("gradient");
+var copyBtn = document.querySelector(".copy-btn");
 function setGradient() {
 	body.style.background = "linear-gradient(to right ," + color1.value + "," + color2.value + ")";
 	css.textContent = body.style.background  + ";";
 }
 color1.addEventListener("input",setGradient);
 color2.addEventListener("input",setGradient);
+
+//? adding event listener to the copy button so that the text will copty to clipboard.
+const copyToClipboard = () => {
+	var copyText = css.textContent;
+	
+	navigator.clipboard.writeText(copyText);
+	// alert("Copied the text: " + copyText);
+}
+copyBtn.addEventListener("click", copyToClipboard);
