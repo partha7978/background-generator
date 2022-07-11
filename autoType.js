550860e6b95eb371eb606533a5e90bb6336b17b5
partha7978 <muduliparthasarathi@gmail.com>
2022-07-11 20:34:12 +0530

    adding autotype text function

---
+++ autoType.js
@@ -0,0 +1,7 @@
+// let typed = new Typed(".autoType", {
+//     strings: ["Partha Sarathi Muduli", "Web Developer", "Software Engineer", "Competitive Coder"],
+//     typeSpeed: 75,
+//     backSpeed: 75,
+//     loop: true
+// });
+
